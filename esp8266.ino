95b4675e1060312f2cf3647a0771ab4aaeedae55
Het Patel <hetexperiment28@gmail.com>
2026-06-23 00:12:37 +0530

    Update esp8266.ino

--- esp8266.ino
+++ esp8266.ino
@@ -1,9 +1,9 @@
 
-//code to get esp data from electricity measurement sensor (for project using potentiometer to fake voltage currents data to shoe live on stage
+//code to get esp data from electricity measurement sensor (for project used potentiometer to fake voltage currents data to show live on stage)
 #define POT_PIN A0   // For ESP32 (use A0 for ESP8266)
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); //remember baud rate for addig in python code. must be same on both side
 }
 
 void loop() {
@@ -14,5 +14,5 @@ void loop() {
 
   Serial.println(energyValue);
 
-  delay(3000);  // send every 3 second
+  delay(3000);  // send every 3 second time stamp
 }
